cdf091e6598792167222d5efa33dadb01ca73b75
doumao <shenren1993@gmail.com>
2025-11-13 17:02:08 +0800

    feat(web): 注册 /index 路由指向 /www/index.html，配合新首页入口

--- coremain/mosdns.go
+++ coremain/mosdns.go
@@ -301,6 +301,20 @@ func (m *Mosdns) initHttpMux() {
 		}
 	}
 
+	// [新增] index 路由 (\"/index\") 的 handler, 指向 /www/index.html
+	indexHandler := func(w http.ResponseWriter, r *http.Request) {
+		data, err := content.ReadFile(\"www/index.html\")
+		if err != nil {
+			m.logger.Error(\"Error reading embedded file\", zap.String(\"file\", \"www/index.html\"), zap.Error(err))
+			http.Error(w, \"Error reading the embedded file\", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set(\"Content-Type\", \"text/html; charset=utf-8\")
+		if _, err := w.Write(data); err != nil {
+			m.logger.Error(\"Error writing response\", zap.Error(err))
+		}
+	}
+
 	plainLogHandler := func(w http.ResponseWriter, r *http.Request) {
 		data, err := content.ReadFile("www/log_plain.html")
 		if err != nil {
@@ -354,6 +368,7 @@ func (m *Mosdns) initHttpMux() {
 	m.httpMux.Get("/", rootHandler)
 	m.httpMux.Get("/graphic", graphicHandler)
 	m.httpMux.Get("/log", logHandler)
+	m.httpMux.Get("/index", indexHandler)
 	m.httpMux.Get("/plog", plainLogHandler)
 	m.httpMux.Get("/rlog", redirectToLog)
 	m.httpMux.Get("/assets/*", staticAssetHandler)
